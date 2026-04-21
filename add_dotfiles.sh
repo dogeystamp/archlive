66a67c63ec3c3c1855a3059251a5dff948f06057
dogeystamp <dogeystamp@disroot.org>
2026-04-21 19:35:51 -0400

    chezmoi: fix up dotfile installation procedure

--- add_dotfiles.sh
+++ add_dotfiles.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
 # Applies dotfiles to the root filesystem.
-# RUN THIS IN THE SAME DIRECTORY AS THIS SCRIPT.
+
+if [ ! -e add_dotfiles.sh ]; then
+    echo Run add_dotfiles.sh in the root directory of the repo.
+    exit 1
+fi
 
 set -e
 
-chezmoi init --verbose --config airootfs/home/julius/.config/chezmoi/chezmoi.toml --destination airootfs/home/julius dogeystamp/dotfiles
-chezmoi update --apply --verbose --config airootfs/home/julius/.config/chezmoi/chezmoi.toml --destination airootfs/home/julius
+export HOME="$(realpath "airootfs/home/julius")"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_CONFIG_HOME="$HOME/.config"
+
+
+CHEZMOI_FLAGS="--verbose --config $XDG_CONFIG_HOME/chezmoi/chezmoi.toml --destination $HOME --source $XDG_DATA_HOME/chezmoi --persistent-state $XDG_CONFIG_HOME/chezmoi/chezmoistate.boltdb"
+
+echo "Deploy path: $HOME"
+echo "Chezmoi flags: $CHEZMOI_FLAGS"
+echo Press ENTER to confirm.
+read
+
+chezmoi init $CHEZMOI_FLAGS https://github.com/dogeystamp/dots
+chezmoi update --apply $CHEZMOI_FLAGS
